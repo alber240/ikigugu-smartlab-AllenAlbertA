0ed1387599533dd2fafb21338c17b9a4c015f9e5
Albert A Allen <bigchicha74@gmail.com>
2025-04-28 14:27:29 +0200

    Day 3: Core logic and MQTT integration working

--- src/DeviceControl.js
+++ src/DeviceControl.js
@@ -1,21 +1,35 @@
 import React, { useState } from "react";
+import mqtt from "mqtt";
+
+const devicesList = ["Light", "Fan", "Projector"];
 
 function DeviceControl() {
-  const [deviceState, setDeviceState] = useState(false);
+    const [deviceStates, setDeviceStates] = useState({
+        Light: false,
+        Fan: false,
+        Projector: false,
+    });
 
-  const toggleDevice = () => {
-    setDeviceState(!deviceState);
-  };
+    const toggleDevice = (device) => {
+        setDeviceStates((prevStates) => ({
+            ...prevStates,
+            [device]: !prevStates[device],
+        }));
+    };
 
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px"}}>
-      <h1>Smart Lab Manager</h1>
-      <p> Device is {deviceState ? "ON" : "OFF"}</p>
-      <button onClick={toggleDevice} style={{ padding: "10px", fontSize: "16px"}}> 
-        {deviceState ? "Turn OFF" : "Turn ON"}
-      </button>
-    </div>
-  );
+    return (
+        <div style={{ textAlign: "center", marginTop: "50px"}}>
+           <h1>Smart Lab Manager</h1>
+           {devicesList.map((device) => (
+            <div key={device} style={{ margin: "20px" }}>
+            <p>{device} is {deviceStates[device] ? "ON" : "OFF"} </p>
+            <button onClick={() => toggleDevice(device)} style={{ padding: "10px", fontSize: "16px" }}>
+                {deviceStates[device] ? "Turn OFF" : "Turn ON"}
+            </button>
+            </div>
+        ))}
+        </div>
+    );
 }
 
 export default DeviceControl;
